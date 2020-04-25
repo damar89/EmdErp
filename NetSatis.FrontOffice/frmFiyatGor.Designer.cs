57636e1916c1ffb0eca620bbcdd382489a9b88f0
MEHMET DAMAR <mehmetdamar89@hotmail.com>
2020-04-26 02:18:20 +0300

    tttt

--- NetSatis.FrontOffice/frmFiyatGor.Designer.cs
+++ NetSatis.FrontOffice/frmFiyatGor.Designer.cs
@@ -46,7 +46,8 @@
             // txtBarkod
             // 
             this.txtBarkod.EditValue = "";
-            this.txtBarkod.Location = new System.Drawing.Point(99, 44);
+            this.txtBarkod.Location = new System.Drawing.Point(115, 54);
+            this.txtBarkod.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtBarkod.Name = "txtBarkod";
             this.txtBarkod.Properties.Appearance.BackColor = System.Drawing.Color.Lime;
             this.txtBarkod.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
@@ -58,7 +59,7 @@
             this.txtBarkod.Properties.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
             this.txtBarkod.Properties.AutoHeight = false;
             this.txtBarkod.Properties.NullText = "54564";
-            this.txtBarkod.Size = new System.Drawing.Size(414, 36);
+            this.txtBarkod.Size = new System.Drawing.Size(483, 44);
             this.txtBarkod.TabIndex = 0;
             this.txtBarkod.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBarkod_KeyDown);
             // 
@@ -71,14 +72,16 @@
             this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.labelControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.labelControl1.Location = new System.Drawing.Point(0, 0);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(521, 32);
+            this.labelControl1.Size = new System.Drawing.Size(608, 39);
             this.labelControl1.TabIndex = 1;
             this.labelControl1.Text = "Aşağıdaki Kutucuğa BARKOD Okutunuz...";
             // 
             // calcSatisFiyat1
             // 
-            this.calcSatisFiyat1.Location = new System.Drawing.Point(154, 149);
+            this.calcSatisFiyat1.Location = new System.Drawing.Point(180, 183);
+            this.calcSatisFiyat1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.calcSatisFiyat1.Name = "calcSatisFiyat1";
             this.calcSatisFiyat1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.calcSatisFiyat1.Properties.Appearance.Options.UseFont = true;
@@ -91,7 +94,7 @@
             this.calcSatisFiyat1.Properties.EditFormat.FormatString = "c2";
             this.calcSatisFiyat1.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.calcSatisFiyat1.Properties.ReadOnly = true;
-            this.calcSatisFiyat1.Size = new System.Drawing.Size(359, 23);
+            this.calcSatisFiyat1.Size = new System.Drawing.Size(419, 28);
             this.calcSatisFiyat1.TabIndex = 6;
             // 
             // labelControl26
@@ -104,9 +107,10 @@
             this.labelControl26.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.labelControl26.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.labelControl26.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.labelControl26.Location = new System.Drawing.Point(12, 151);
+            this.labelControl26.Location = new System.Drawing.Point(14, 186);
+            this.labelControl26.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl26.Name = "labelControl26";
-            this.labelControl26.Size = new System.Drawing.Size(136, 20);
+            this.labelControl26.Size = new System.Drawing.Size(159, 25);
             this.labelControl26.TabIndex = 7;
             this.labelControl26.Text = "Satış Fiyatı :";
             // 
@@ -120,9 +124,10 @@
             this.labelControl4.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.labelControl4.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.labelControl4.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.labelControl4.Location = new System.Drawing.Point(12, 125);
+            this.labelControl4.Location = new System.Drawing.Point(14, 154);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl4.Name = "labelControl4";
-            this.labelControl4.Size = new System.Drawing.Size(136, 20);
+            this.labelControl4.Size = new System.Drawing.Size(159, 25);
             this.labelControl4.TabIndex = 8;
             this.labelControl4.Text = "  Stok Adı :";
             // 
@@ -136,9 +141,10 @@
             this.labelControl2.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.labelControl2.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.labelControl2.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.labelControl2.Location = new System.Drawing.Point(12, 97);
+            this.labelControl2.Location = new System.Drawing.Point(14, 119);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(136, 20);
+            this.labelControl2.Size = new System.Drawing.Size(159, 25);
             this.labelControl2.TabIndex = 9;
             this.labelControl2.Text = "  Stok Kodu :";
             // 
@@ -147,9 +153,10 @@
             this.btnKapat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnKapat.ImageOptions.Image = global::NetSatis.FrontOffice.Properties.Resources.close_32x32;
-            this.btnKapat.Location = new System.Drawing.Point(429, 263);
+            this.btnKapat.Location = new System.Drawing.Point(500, 324);
+            this.btnKapat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnKapat.Name = "btnKapat";
-            this.btnKapat.Size = new System.Drawing.Size(92, 38);
+            this.btnKapat.Size = new System.Drawing.Size(107, 47);
             this.btnKapat.TabIndex = 10;
             this.btnKapat.Text = "Kapat";
             this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
@@ -164,9 +171,10 @@
             this.txtKod.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.txtKod.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.txtKod.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.txtKod.Location = new System.Drawing.Point(154, 97);
+            this.txtKod.Location = new System.Drawing.Point(180, 119);
+            this.txtKod.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtKod.Name = "txtKod";
-            this.txtKod.Size = new System.Drawing.Size(355, 20);
+            this.txtKod.Size = new System.Drawing.Size(414, 25);
             this.txtKod.TabIndex = 9;
             // 
             // txtStokAdi
@@ -179,24 +187,26 @@
             this.txtStokAdi.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.txtStokAdi.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.txtStokAdi.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.txtStokAdi.Location = new System.Drawing.Point(154, 125);
+            this.txtStokAdi.Location = new System.Drawing.Point(180, 154);
+            this.txtStokAdi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtStokAdi.Name = "txtStokAdi";
-            this.txtStokAdi.Size = new System.Drawing.Size(355, 20);
+            this.txtStokAdi.Size = new System.Drawing.Size(414, 25);
             this.txtStokAdi.TabIndex = 9;
             // 
             // labelControl3
             // 
             this.labelControl3.Appearance.BorderColor = System.Drawing.Color.Silver;
-            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.labelControl3.Appearance.Options.UseBorderColor = true;
             this.labelControl3.Appearance.Options.UseFont = true;
             this.labelControl3.Appearance.Options.UseTextOptions = true;
             this.labelControl3.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.labelControl3.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.labelControl3.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.labelControl3.Location = new System.Drawing.Point(9, 178);
+            this.labelControl3.Location = new System.Drawing.Point(10, 257);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(136, 20);
+            this.labelControl3.Size = new System.Drawing.Size(159, 25);
             this.labelControl3.TabIndex = 9;
             this.labelControl3.Text = "Mevcut Stok Mikarı :";
             // 
@@ -210,16 +220,17 @@
             this.lblMiktar.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
             this.lblMiktar.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
             this.lblMiktar.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.lblMiktar.Location = new System.Drawing.Point(154, 178);
+            this.lblMiktar.Location = new System.Drawing.Point(180, 257);
+            this.lblMiktar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lblMiktar.Name = "lblMiktar";
-            this.lblMiktar.Size = new System.Drawing.Size(355, 20);
+            this.lblMiktar.Size = new System.Drawing.Size(414, 25);
             this.lblMiktar.TabIndex = 9;
             // 
             // frmFiyatGor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 303);
+            this.ClientSize = new System.Drawing.Size(608, 373);
             this.Controls.Add(this.btnKapat);
             this.Controls.Add(this.labelControl26);
             this.Controls.Add(this.labelControl4);
@@ -234,6 +245,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.IconOptions.ShowIcon = false;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmFiyatGor";
