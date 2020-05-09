bb292da06ec03eff9835c72bda96d53380aa2fd6
MEHMET DAMAR <mehmetdamar89@hotmail.com>
2020-05-10 00:51:43 +0300

    rapor2

--- NetSatis.BackOffice/Raporlar/frmGunlukHareketler.cs
+++ NetSatis.BackOffice/Raporlar/frmGunlukHareketler.cs
@@ -1,7 +1,13 @@
-﻿namespace NetSatis.BackOffice.Raporlar
+﻿using NetSatis.Entities.Context;
+using NetSatis.Entities.Data_Access;
+
+namespace NetSatis.BackOffice.Raporlar
 {
     public partial class frmGunlukHareketler : DevExpress.XtraEditors.XtraForm
     {
+        NetSatisContext context = new NetSatisContext();
+        FisDAL fisDal = new FisDAL();
+        KasaHareketDAL kasaHareketDal = new KasaHareketDAL();
         public frmGunlukHareketler()
         {
             InitializeComponent();
